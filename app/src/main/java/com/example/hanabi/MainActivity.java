6034517dfa6956e574d3eb25b96ce9b454289446
seobok <tjqhrals99@naver.com>
2022-10-26 19:55:09 +0900

    Firebase 연동
    서복민 Firebase 연동

---
+++ app/src/main/java/com/example/hanabi/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hanabi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+}
