1d1f7fae32c4f5df943145ab7cfd47cc46dc3492
ar <wjs505@gmail.com>
2022-11-20 16:18:04 +0900

    card func
    전도현 card func

--- app/src/main/java/com/example/hanabi/InGameActivity.java
+++ app/src/main/java/com/example/hanabi/InGameActivity.java
@@ -4,12 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,6 +20,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
@@ -37,40 +41,124 @@ public class InGameActivity extends AppCompatActivity {
     card_info[] right_card = new card_info[5] ;
     card_info[] new_card = new card_info[5] ;
     card_info[] my_card = new card_info[5] ;
+    card_info[] score_deck = new card_info[5] ;
 
     ImageView left_image[] = new ImageView[5] ;
     Integer[] Rid_left = { R.id.l1_image , R.id.l2_image , R.id.l3_image , R.id.l4_image , R.id.l5_image } ;
 
+    TextView l_number[] = new TextView[5] ;
+    Integer[] Rid_l_n = { R.id.l1_number , R.id.l2_number , R.id.l3_number , R.id.l4_number , R.id.l5_number } ;
+
     ImageView right_image[] = new ImageView[5] ;
     Integer[] Rid_right = { R.id.r1_image , R.id.r2_image , R.id.r3_image , R.id.r4_image , R.id.r5_image } ;
 
+    TextView r_number[] = new TextView[5] ;
+    Integer[] Rid_r_n = { R.id.r1_number , R.id.r2_number , R.id.r3_number , R.id.r4_number , R.id.r5_number } ;
+
     ImageView my_image[] = new ImageView[5] ;
     Integer[] Rid_my = { R.id.m1_image , R.id.m2_image , R.id.m3_image , R.id.m4_image , R.id.m5_image } ;
+
+    TextView m_number[] = new TextView[5] ;
+    Integer[] Rid_m_n = { R.id.m1_number , R.id.m2_number , R.id.m3_number , R.id.m4_number , R.id.m5_number } ;
+
+    ImageView score_image[] = new ImageView[5] ;
+    Integer[] Rid_score = { R.id.s1_image , R.id.s2_image , R.id.s3_image , R.id.s4_image , R.id.s5_image } ;
 
     Button step1_info, step1_submission;
     Button step2_throw, step2_sub, step2_left_player, step2_right_player;
     Button step3_number_1, step3_number_2, step3_number_3, step3_number_4, step3_number_5;
     Button step4_color, step4_number;
-    Button.OnClickListener step1_O, step_number;
+    Button.OnClickListener step1_O, step_number , step4_0 ;
 
 
     Boolean step4_check;
-    int card_num, player, color_or_number;
+
+    int life = 3 ;
+    int card_num, player ;
+    // player left : 1 , right : 2
     int sub_or_throw ;
+    int left_player , right_player , my_id ;
     String temp_string ;
 
     LinearLayout step1, step2_submission, step2_info, step4, step3_number;
 
-    // 0 : red , 1 : blue , 2 : white , 3 : yellow , 4 : green
-    String[] color_array = { "red_" , "blue_" , "white_" , "yellow_" , "green_" } ;
+    // 0 : red , 1 : blue , 2 : white , 3 : yellow , 4 : green 5 : ~
+    String[] color_array = { "red_" , "blue_" , "white_" , "yellow_" , "green_" , "card" } ;
 
-
-    public void CARD_THROW(int card_num) { // 내 카드 버리기
+    public void CARD_THROW( ) { // 내 카드 버리기
 
 
     }
 
-    public void CARD_SUB( int card_num ) { // 내 카드 제출
+    public void CARD_SUB( ) { // 내 카드 제출
+
+        card_info sub_card = my_card[ card_num ] ;
+
+        int color = sub_card.Color() ;
+        int number = sub_card.Number() ;
+
+        if( score_deck[ color ].Number() + 1 == number ) { // O
+            score_deck[ color ].Set( color , number + 1 );
+
+            String temp_string = color_array[ score_deck[ color ].Color() ] + score_deck[ color ].Number() ;
+            int resID = getResId( temp_string , R.drawable.class); // or other resource class
+            score_image[ color ].setImageResource( resID ) ;
+
+        }
+        else { // X
+
+        }
+
+    }
+
+    public void INFO_CARD() { // 색 정보
+
+        // 0 : red , 1 : blue , 2 : white , 3 : yellow , 4 : green 5 : ~
+        if( player == 1 ){ // left
+
+            int i ;
+            int set_color = left_card[ card_num ].Color() ;
+
+            if( set_color == 0 ) for( i = 0 ; i < 5 ; i ++ ) if( left_card[ i ].Color() == set_color ) l_number[ i ].setBackgroundColor( Color.RED ) ;
+            else if( set_color == 1 ) for( i = 0 ; i < 5 ; i ++ ) if( left_card[ i ].Color() == set_color ) l_number[ i ].setBackgroundColor( Color.BLUE ) ;
+            else if( set_color == 2 ) for( i = 0 ; i < 5 ; i ++ ) if( left_card[ i ].Color() == set_color ) l_number[ i ].setBackgroundColor( Color.WHITE ) ;
+            else if( set_color == 3 ) for( i = 0 ; i < 5 ; i ++ ) if( left_card[ i ].Color() == set_color ) l_number[ i ].setBackgroundColor( Color.YELLOW ) ;
+            else if( set_color == 4 ) for( i = 0 ; i < 5 ; i ++ ) if( left_card[ i ].Color() == set_color ) l_number[ i ].setBackgroundColor( Color.GREEN ) ;
+
+        }
+        else { // right
+
+            int i ;
+            int set_color = right_card[ card_num ].Color() ;
+
+            if( set_color == 0 ) for( i = 0 ; i < 5 ; i ++ ) if( right_card[ i ].Color() == set_color ) r_number[ i ].setBackgroundColor( Color.RED ) ;
+            else if( set_color == 1 ) for( i = 0 ; i < 5 ; i ++ ) if( right_card[ i ].Color() == set_color ) r_number[ i ].setBackgroundColor( Color.BLUE ) ;
+            else if( set_color == 2 ) for( i = 0 ; i < 5 ; i ++ ) if( right_card[ i ].Color() == set_color ) r_number[ i ].setBackgroundColor( Color.WHITE ) ;
+            else if( set_color == 3 ) for( i = 0 ; i < 5 ; i ++ ) if( right_card[ i ].Color() == set_color ) r_number[ i ].setBackgroundColor( Color.YELLOW ) ;
+            else if( set_color == 4 ) for( i = 0 ; i < 5 ; i ++ ) if( right_card[ i ].Color() == set_color ) r_number[ i ].setBackgroundColor( Color.GREEN ) ;
+
+        }
+
+    }
+
+    public void INFO_NUMBER() { // 숫자 정보
+
+        if( player == 1 ){ // left
+
+            int i ;
+            Integer set_number = left_card[ card_num ].Number() ;
+
+            for( i = 0 ; i < 5 ; i ++ ) if( set_number == left_card[ i ].Number() ) l_number[ i ].setText( set_number.toString() );
+
+        }
+        else { // right
+
+            int i ;
+            Integer set_number = right_card[ card_num ].Number() ;
+
+            for( i = 0 ; i < 5 ; i ++ ) if( set_number == right_card[ i ].Number() ) r_number[ i ].setText( set_number.toString() );
+
+        }
 
 
     }
@@ -115,8 +203,7 @@ public class InGameActivity extends AppCompatActivity {
                 number = "4";
             else
                 number = "5";
-            cardList[i].number = number;
-
+            cardList[i].number = number ;
             cardList[i].position = "deck";
             cardList[i].handPosition = "";
         }
@@ -125,6 +212,7 @@ public class InGameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.in_game);
 
@@ -137,12 +225,28 @@ public class InGameActivity extends AppCompatActivity {
         boardRef = roomRef.child("Board");
         // -->
 
+
         //<-- get current player id
         String[] email = firebaseUser.getEmail().split("@",2);
         id = email[0];
         //-->
 
         initCardList();
+
+        // id
+        if( my_id == 1 ){
+            right_player = 3 ;
+            left_player = 2 ;
+        }
+        else if( my_id == 2 ){
+            left_player = 3 ;
+            right_player = 1 ;
+        }
+        else {
+            left_player = 1 ;
+            right_player = 2 ;
+        }
+        //
 
         boardRef.addChildEventListener(new ChildEventListener() {
             @Override
@@ -173,25 +277,26 @@ public class InGameActivity extends AppCompatActivity {
             }
         });
 
-        temp_string = color_array[ 1 ] + "1" ;
 
         for( int i = 0 ; i <  5 ; i ++ ) {
 
             my_card[ i ] = new card_info() ;
             right_card[ i ] = new card_info() ;
             left_card[ i ] = new card_info() ;
-            new_card[ i ] = new card_info() ;
+
             left_image[ i ] = (ImageView) findViewById(Rid_left[ i ] ) ;
+            l_number[ i ] = (TextView) findViewById(Rid_l_n[ i ] ) ;
+
             right_image[ i ] = (ImageView) findViewById(Rid_right[ i ] ) ;
+            r_number[ i ] = (TextView) findViewById(Rid_r_n[ i ] ) ;
+
             my_image[ i ] = (ImageView) findViewById(Rid_my[ i ] ) ;
+            m_number[ i ] = (TextView) findViewById(Rid_m_n[ i ] ) ;
+            my_image[ i ].setImageResource( R.drawable.card ) ;
+
+            score_image[ i ] = (ImageView) findViewById(Rid_score[ i ] ) ;
 
         }
-
-
-        left_card[ 0 ].Set( 1 , 2 ) ;
-        right_card[ 0 ].Set( 1 , 2 ) ;
-        my_card[ 0 ].Set( 3 , 1 ) ;
-
 
         temp_string = color_array[ left_card[ 0 ].Color() ] + left_card[ 0 ].Number() ;
         int resID = getResId( temp_string , R.drawable.class); // or other resource class
@@ -230,7 +335,7 @@ public class InGameActivity extends AppCompatActivity {
                     case R.id.step1_info:
                         step2_info.setVisibility(View.VISIBLE);
                         step1.setVisibility(View.INVISIBLE);
-                        card_num = player = color_or_number = -1 ;
+                        card_num = player = -1 ;
                         break;
                     case R.id.step1_submission:
                         step2_submission.setVisibility(View.VISIBLE);
@@ -291,12 +396,32 @@ public class InGameActivity extends AppCompatActivity {
                     step4.setVisibility(View.VISIBLE);
                 else if( sub_or_throw == -1 ) { // 카드 버림
 
+                    CARD_THROW();
+
                 }
                 else if( sub_or_throw == 1 ) { // 카드 제출
 
+                    CARD_SUB() ;
                 }
 
             }
+        };
+
+        step4_0 = new View.OnClickListener(){
+
+            public void onClick(View view) {
+
+                step4.setVisibility(View.INVISIBLE);
+                switch (view.getId()) {
+                    case R.id.step4_color:
+                        INFO_CARD();
+                        break;
+                    case R.id.step4_number:
+                        INFO_NUMBER();
+                        break;
+                    }
+            }
+
         };
 
         step1_info.setOnClickListener(step1_O);
@@ -313,6 +438,8 @@ public class InGameActivity extends AppCompatActivity {
         step3_number_3.setOnClickListener(step_number) ;
         step3_number_4.setOnClickListener(step_number) ;
         step3_number_5.setOnClickListener(step_number) ;
+        step4_color.setOnClickListener(step4_0);
+        step4_number.setOnClickListener(step4_0);
 
     }
 
@@ -322,10 +449,13 @@ class card_info {
 
     private int color = 0; // 0 : red , 1 : blue , 2 : white , 3 : yellow , 4 : green
     private int number = 0;
+    private int idx ;
 
-    public void Set(int a, int b) {
-        color = a;
-        number = b;
+    public void set_idx( int Idx ) { idx = Idx ; }
+
+    public void Set( int Color , int Number ) {
+        color = Color ;
+        number = Number ;
     }
 
     public int Color() {
@@ -335,6 +465,8 @@ class card_info {
     public int Number() {
         return number ;
     }
+
+    public int Idx(){ return idx ; }
 
 }
 
